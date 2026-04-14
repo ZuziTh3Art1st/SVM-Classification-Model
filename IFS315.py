d08665a6c9c9128c966e0fcc0356ec2e2c1fdd58
Ashley Tinotenda Buzuzi <4465983@myuwc.ac.za>
2026-04-14 21:59:37 +0200

    Update IFS315.py
    The description tab has been updated.

--- IFS315.py
+++ IFS315.py
@@ -120,18 +120,20 @@ with st.expander("What am I looking at?"):
     The data used to make this folder is within the GitHub folder and values have been normalized to fit a 0 to 1 scale called Minimum-Maximum Scaler.
 
     **The filters observed**
-    - Frequent Flyer Miles (Amount Travelled)
+    1.  Frequent Flyer Miles (Amount Travelled)
     
-    -The amount of hours spent gaming
+    2.  The amount of hours spent gaming
     
-    -The amount of Ice Cream litres consumed
+    3.  The amount of Ice Cream litres consumed
     
     **The Scatter Plots:** 
     1. The **3D Plot** shows how often a person travels, how long is a game played and ice creaam consumed interacts in a three dimensional space.
 
     
     2. The **2D Plots** shows the real lines or decision boundaries the SVM drew to seperate the filters in sets of two to accurately show the relationships between them.
-    
+
+
+    The 3D plots show the real relationship between the 3 filers but having 2d graphs comparing two variables a time helps in the ease of identifiying patterns
     **FINAL RESULTS**
     Ice Cream has no effect on what makes a perfect match for the user.
     The amount travlled usually makes for a perfect match but there must be a balancing act where time spent gaming is mid range in terms of value.
